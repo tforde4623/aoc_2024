02b24377a0a8170996a96dcc335098548eb3e2d8
Tommy Forde <sltebr33zy@gmail.com>
2024-12-05 17:13:33 +0000

    WIP: dev container setup

--- test/day_1.exs
+++ test/day_1.exs
@@ -1,6 +1,6 @@
-defmodule Aoc2024ElixirTest do
+defmodule Day1 do
   use ExUnit.Case
-  doctest Aoc2024Elixir
+  # doctest Aoc2024Elixir
 
   test "greets the world" do
     assert Aoc2024Elixir.hello() == :world
