b73b2092419cdc8be55fb4a2356fc46b477d6275
Tommy Forde <sltebr33zy@gmail.com>
2024-12-10 18:14:05 +0000

    day 3 part 2

--- lib/day_3.ex
+++ lib/day_3.ex
@@ -1,12 +1,35 @@
 defmodule Day3 do
 
+  # matches mul(x,y) statements to ["mul(x,y)", "x", "y"]
   @mul_regex ~r/mul\((\d{1,3}),\s*(\d{1,3})\)/
+  # continues to match mul(x,y) statement like @mul_regex, also matches do() and don't()
+  @cond_mul_regex ~r/(mul\((\d{1,3}),\s*(\d{1,3})\)|do\(\)|don't\(\))/
 
   def part1(file_path) do
     Regex.scan(@mul_regex, File.read!(file_path))
     |> Enum.reduce(0, fn [_, x, y], acc ->
       acc + String.to_integer(x) * String.to_integer(y)
     end)
+  end
+
+  def part2(file_path) do
+    Regex.scan(@cond_mul_regex, File.read!(file_path))
+    |> Enum.map(fn
+          [_, "mul(" <> _ = match, x, y] -> [match, x, y] # mul(x, y)
+          [_, match] -> match # do() or don't()
+        end)
+    |> Enum.reduce({0, :do}, fn op, {sum, prev_cond} ->
+      case op do
+        [_, x, y] -> if prev_cond == :do do
+          {sum + String.to_integer(x) * String.to_integer(y), prev_cond}
+        else
+          {sum, prev_cond}
+        end
+        "do()" -> {sum, :do}
+        "don't()" -> {sum, :dont}
+      end
+    end)
+    |> elem(0)
   end
 
 end
--- test/day_3_test.exs
+++ test/day_3_test.exs
@@ -2,8 +2,12 @@ defmodule Day3Test do
 
   use ExUnit.Case
 
-  test "part1/0" do
+  test "part1/1" do
     assert Day3.part1("./inputs/test/test_input_3.txt") == 161
+  end
+
+  test "part2/1" do
+    assert Day3.part2("./inputs/test/test_input_3_2.txt") == 48
   end
 
 end
